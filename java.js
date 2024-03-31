ba5b003ea3975b3d020008790cc4e863e021cf7d
kaushal <153196042+kaushal-kumar-2109@users.noreply.github.com>
2024-03-31 18:10:51 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+function down0(){
+    let down=document.getElementById("allparts0");
+    down.classList.remove("up");
+    down.classList.add("down");
+    let up=document.getElementById("up0");
+}
+function up0(){
+    let down=document.getElementById("allparts0");
+    down.classList.remove("down");
+    down.classList.add("up");
+}
